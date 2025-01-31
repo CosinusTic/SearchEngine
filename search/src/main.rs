be47f776cf414b51405037e8b6d8c2e40bb86b54
nathan.delmarche <nathan.delmarche@epita.fr>
2025-01-31 22:51:52 +0100

    FIX: slight modification on flow in main

--- search/src/main.rs
+++ search/src/main.rs
@@ -1,19 +1,18 @@
 use common::data::{CLIArgs, Url};
 
 fn main() {
-    let url_string = String::from("www.example.com");
     let score = 10;
     let raw: Vec<String> = std::env::args().collect();
     if let Some(parsed_args) = CLIArgs::new(raw) {
         println!("Arguments parsed: url={:?}, depth={:?}", parsed_args.url, parsed_args.crawling_depth);
+        if let Some(url) = Url::new(parsed_args.url, score) {
+            url.print_url(); 
+        } else {
+            println!("Invalid URL");
+        }
     }
     else {
         println!("Arguments passed are of the wrong kind");
-    }
-    if let Some(url) = Url::new(url_string, score) {
-        url.print_url(); 
-    } else {
-        println!("Invalid URL");
     }
 }
 
